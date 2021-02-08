2698286cb41a0ff2e96555d5119f578690f20cfb
degoxec98 <degoxec98@gmail.com>
2021-02-08 00:34:09 -0500

    puntos faciles

--- PuntosFaciales.py
+++ PuntosFaciales.py
@@ -20,33 +20,42 @@ export QT_SCREEN_SCALE_FACTORS=1
 export QT_SCALE_FACTOR=1
 """
 
+pointsX = []
+pointsY = []
 
-#Carga el detector
-detector = dlib.get_frontal_face_detector()
+detector = dlib.get_frontal_face_detector() #Carga el detector
 
-#Lee la imagen
-img = cv2.imread("face.jpg")
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") #Carga el predictor (predice)
 
-#Convierte la imagen en escala de grises
-gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+img = cv2.imread("face.jpg") #Lee la imagen
 
-#Usa el detector para encontrar puntos de referencia
-faces = detector(gray)
+gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY) #Convierte la imagen en escala de grises
+
+faces = detector(gray) #Usa el detector para encontrar puntos de referencia
 
 for face in faces:
     x1 = face.left() #Punto izquierdo
     y1 = face.top() #Punto superior
     x2 = face.right() #Punto derecho
     y2 = face.bottom() #Punto de botón
-    # Dibuja el rectangulo
-    cv2.rectangle(img=img, pt1=(x1, y1), pt2=(x2, y2), color=(0, 255, 0), thickness=4)
+    # cv2.rectangle(img=img, pt1=(x1, y1), pt2=(x2, y2), color=(0, 255, 0), thickness=4) # Dibuja el rectangulo
+    # Busca los puntos de referencia
+    landmarks = predictor(image=gray, box=face)
+    for n in range(0, 68):
+        x = landmarks.part(n).x
+        y = landmarks.part(n).y
+        #Dibuja el circulo
+        cv2.circle(img=img, center=(x, y), radius=2, color=(0, 255, 0), thickness=-1)
+        #Restaca los puntos(X y Y) de la cara
+        pointsX.append(x)
+        pointsY.append(y)
 
-#Muestra la imagen
-cv2.imshow(winname="Face", mat=img)
+print("Puntos X: ", pointsX)
+print("Puntos Y: ", pointsY)
 
-#Espera para salir
-cv2.waitKey(delay=0)
+cv2.imshow(winname="Face", mat=img) #Muestra la imagen
 
-#Cierra todas las ventanas
-cv2.destroyAllWindows()
+cv2.waitKey(delay=0) #Espera para salir
+
+cv2.destroyAllWindows() #Cierra todas las ventanas
 
